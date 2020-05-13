bdb8b4fbb6255d5fc48277fd43374d678b9b4b24
famesensor <famfew25@gmail.com>
2020-05-14 01:00:53 +0700

    [update] import multer , upload image

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-// Import Models 
+// Import Models
 const User = require('../../models/user');
 const Post = require('../../models/post');
 
@@ -10,80 +10,99 @@ const Post = require('../../models/post');
 const validateEditProfile = require('../../validator/edit');
 const validateImageURL = require('../../validator/image');
 
+// import function
+const multer = require('../../utils/multer/multer');
+const {
+    uploadImageToStorage
+} = require('../../utils/firebase-upload/uploadimg');
 // @route       GET api/profile/test
 // @desc        Default Route
 // @access      Public
-router.get('/test', (req, res) => res.json({ msg : 'Tests profile system' }));
+router.get('/test', (req, res) => res.json({ msg: 'Tests profile system' }));
 
 // @route   GET api/profile/
-// @desc    Get current users 
+// @desc    Get current users
 // @access  Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-    // let errors = {}; 
-    User.findById(req.user.id) 
-        .then((profile) => {
-            res.json(profile)
-        })
-        .catch((err) => {
-            res.status(404).json({ profile: 'User not found'});
-        });
-});
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        // let errors = {};
+        User.findById(req.user.id)
+            .then((profile) => {
+                res.json(profile);
+            })
+            .catch((err) => {
+                res.status(404).json({ profile: 'User not found' });
+            });
+    }
+);
 
 // @route   GET api/profile/alluser
-// @desc    Get All users 
+// @desc    Get All users
 // @access  Public
 router.get('/alluser', (req, res) => {
-    User.find({ status: 0 }).sort({ created: -1 })
+    User.find({ status: 0 })
+        .sort({ created: -1 })
         .then((profile) => {
             if (profile.length === 0) {
-                return res.status(200).json({ msg : 'User not found' });
+                return res.status(200).json({ msg: 'User not found' });
             }
             res.json(profile);
         })
         .catch((err) => {
-            res.sendStatus(500)
-        })
+            res.sendStatus(500);
+        });
 });
 
 // @route   POST api/profile/edit
 // @desc    post edit users profile
 // @access  Private
-router.post('/edit', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const { errors, isValid } = validateEditProfile(req.body);
-    
-    // Check Validation
-    if (!isValid) {
-        // console.log(errors);
-        return res.status(400).json(errors);
-    }
+router.post(
+    '/edit',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateEditProfile(req.body);
 
-    User.findById(req.user.id) 
-        .then((userData) => { 
-            userData.phone = req.body.phone;
-            userData.aboutMe = req.body.about;
-            
-            userData.save().then((user) => {
-                res.json(user);
+        // Check Validation
+        if (!isValid) {
+            // console.log(errors);
+            return res.status(400).json(errors);
+        }
+
+        User.findById(req.user.id)
+            .then((userData) => {
+                userData.phone = req.body.phone;
+                userData.aboutMe = req.body.about;
+
+                userData.save().then((user) => {
+                    res.json(user);
+                });
             })
-        })
-        .catch((err) => {
-            res.status(404).json({ error: 'No Post found with that ID'});
-        });
-})
+            .catch((err) => {
+                res.status(404).json({ error: 'No Post found with that ID' });
+            });
+    }
+);
 
 // @route   DELETE api/profile/delete
 // @desc    Get current users profile
 // @access  Private
-router.delete('/delete', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Post.findOneAndRemove({ user: req.user.id}).then(
-        User.findByIdAndDelete({ _id: req.user.id }).then(
-            res.json({ success: true })
-        )
-        .catch((err) => {
-            res.status(404).json({ profile: 'No User found with that ID'})
-        })
-    )
-});
+router.delete(
+    '/delete',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Post.findOneAndRemove({ user: req.user.id }).then(
+            User.findByIdAndDelete({ _id: req.user.id })
+                .then(res.json({ success: true }))
+                .catch((err) => {
+                    res.status(404).json({
+                        profile: 'No User found with that ID'
+                    });
+                })
+        );
+    }
+);
 
 // @route   GET api/profile/handle/:id
 // @desc    Get the profile data of the params passed
@@ -91,44 +110,51 @@ router.delete('/delete', passport.authenticate('jwt', { session: false }), (req,
 router.get('/handle/:id', (req, res) => {
     let errors = {};
 
-    User.findOne({ username: req.parms.id }).select({ state: 0, terms: 0, Card: 0})
+    User.findOne({ username: req.parms.id })
+        .select({ state: 0, terms: 0, Card: 0 })
         .then((profile) => {
             if (!profile) {
                 errors.msg = 'User not found';
-                return res.status(404).json(errors)
+                return res.status(404).json(errors);
             }
-            res.json(profile)
+            res.json(profile);
         })
         .catch((err) => {
-            res.state(404).json({ profile: 'No User found with that ID'});
+            res.state(404).json({ profile: 'No User found with that ID' });
         });
 });
 
 // @route   POST api/profile/upload
-// @desc    Post Picture user 
+// @desc    Post Picture user
 // @access  Private
-router.post('/upload', passport.authenticate('jwt', { session: false }), (req, res ) => {
-    const { errors, isValid } = validateImageURL(req.body);
+router.post(
+    '/upload',
+    passport.authenticate('jwt', { session: false }),
+    multer.single('file'),
+    (req, res) => {
+        const { errors, isValid } = validateImageURL(req.body);
 
-    if (!isValid) {
-        return res.status(400).json(errors);
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        User.findById(req.user.id)
+            .then((newImage) => {
+                newImage.photo_user = req.body.imageURL;
+
+                newImage
+                    .save()
+                    .then((user) => {
+                        res.json(user);
+                    })
+                    .catch((err) => {
+                        res.status(400).json({ image: 'Upload fail' });
+                    });
+            })
+            .catch((err) => {
+                res.state(404).json({ image: 'No User found with that ID' });
+            });
     }
-    
-    User.findById(req.user.id)
-        .then((newImage) => {
-            newImage.photo_user = req.body.imageURL
-
-            newImage.save()
-                .then((user) => {
-                    res.json(user)
-                })
-                .catch((err) => {
-                    res.status(400).json({ image : "Upload fail" })
-                })
-        })
-        .catch((err) => {
-            res.state(404).json({image: 'No User found with that ID'});
-        })
-});
+);
 
 module.exports = router;
